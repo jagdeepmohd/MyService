97d0b8887fa75b74696382cdadeb15f9a231bb28
CORP\jmohmmed <jagdeep.mohmmed@capgemini.com>
2018-02-11 14:19:21 +0530

    Timer task calling Service of App 1 again and again and passing data.

--- app/src/main/java/com/app/myservice/OnlyService.java
+++ app/src/main/java/com/app/myservice/OnlyService.java
@@ -1,6 +1,7 @@
 package com.app.myservice;
 
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
@@ -16,8 +17,11 @@ import java.util.TimerTask;
 
 public class OnlyService extends Service {
     Handler mhandler=new Handler();
-    private static final long interval = 40000;
+    private static final long interval = 10000;
     private Timer timer=null;
+    private int count=0;
+    Intent i = new Intent();
+    ComponentName compo = new ComponentName("com.example.nouiapplication", "com.example.nouiapplication.service.MyService");
     @Override
     public void onCreate() {
         if(timer!=null)
@@ -29,17 +33,19 @@ public class OnlyService extends Service {
          timer=new Timer();
             timer.scheduleAtFixedRate(new ToastTimerTask(),0,interval);
         }
-        super.onCreate();
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
+        super.onCreate();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
+        timer.cancel();
         super.onDestroy();
     }
 
@@ -56,7 +62,12 @@ public class OnlyService extends Service {
             mhandler.post(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(getApplicationContext(), " 2nd App OnlyService running", Toast.LENGTH_SHORT).show();
+                    //Calculate Signal strength and pass it to App 1 (OnstartCommand).
+                    //Do Calculations - Like get Telephony data and send it back to first service
+        i.putExtra("data","RETURNING BACK DATA"+ ++count);
+        i.setComponent(compo);
+        getApplicationContext().startService(i);
+                 //   Toast.makeText(getApplicationContext(), " Second App  : "+ ++count, Toast.LENGTH_SHORT).show();
                 }
             });
 
